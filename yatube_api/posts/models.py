8c3cbe79c70afba3786dbc4c3692641858d641ef
Тимур Даянов <tima.skye@yandex.ru>
2025-07-08 23:06:22 +0500

    Создание моделей, описание мета классов

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,23 +4,58 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    """Модель Группы."""
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+
 class Post(models.Model):
+    """Модель Публикации."""
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, related_name='posts', null=True, blank=True)
 
     def __str__(self):
         return self.text
 
+    class Meta:
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ['-pub_date']
+
 
 class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+    """Модель Комментарии."""
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
-    created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+    created = models.DateTimeField('Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['created']
+
+
+class Follow(models.Model):
+    """Модель Подписки."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follows')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(fields=('user', 'following'), name='unique_user_following')
+        ]
