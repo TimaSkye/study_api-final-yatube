a162f7a90498effde5a1a624eddaf6fcedecc10b
Тимур Даянов <tima.skye@yandex.ru>
2025-07-08 23:06:55 +0500

    Настройка роутов проекта

--- yatube_api/api/v1/urls.py
+++ yatube_api/api/v1/urls.py
@@ -1,3 +1,22 @@
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from api.v1.views import PostViewSet, GroupViewSet, FollowViewSet, CommentViewSet
+
+v1_router = routers.DefaultRouter()
+v1_router.register(r'posts', PostViewSet)
+v1_router.register(r'groups', GroupViewSet)
+v1_router.register(r'follow', FollowViewSet)
+v1_router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
 urlpatterns = [
-    
+    path('', include(v1_router.urls)),
+    path('jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
