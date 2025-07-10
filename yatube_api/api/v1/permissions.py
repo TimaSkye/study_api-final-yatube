1e9d80b7b2583fa9044ea9c64e3e61cbe641023c
Тимур <147384709+TimaSkye@users.noreply.github.com>
2025-07-10 16:03:03 +0500

    2 ревью

--- yatube_api/api/v1/permissions.py
+++ yatube_api/api/v1/permissions.py
@@ -7,9 +7,6 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     остальные пользователи имеют только права на чтение.
     """
 
-    def has_permission(self, request, view):
-        return True
-
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
